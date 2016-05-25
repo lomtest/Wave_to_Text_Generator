7199ddf7a6be8544fe77bddd895511496c59e120
Nils Edvardsson <nils@nilsjc.com>
2016-05-25 09:51:16 +0200

    Add files via upload

--- SquareWave.cs
+++ SquareWave.cs
@@ -11,23 +11,30 @@ namespace WaveFormGenerator
         string returnWave { get; set; }
         public string generate(string maxSamples, string maxLevel, string multiply)
         {
+            var _multiply = int.Parse(multiply);
             var _maxSamples = Int32.Parse(maxSamples);
             var _maxLevel = Int32.Parse(maxLevel);
+            _maxSamples /= _multiply;
             var _half = _maxSamples / 2;
-            for (double i = 0; i < _maxSamples; i += 1)
+            for (int j = 0; j < _multiply; j++)
             {
-                if (i < _half)
-                {
-                    returnWave += _maxLevel;
-                    returnWave += ",\n";
-                }
-                else
-                {
-                    returnWave += 0 - _maxLevel;
-                    returnWave += ",\n";
-                }
+
+                for (double i = 0; i < _maxSamples; i += 1)
+                         {
+                             if (i < _half)
+                             {
+                                returnWave += _maxLevel;
+                                returnWave += ",\n";
+                             }
+                             else
+                             {
+                                returnWave += 0 - _maxLevel;
+                                returnWave += ",\n";
+                             }
                 
+                        }
             }
+            
             returnWave = returnWave.Replace("\n", System.Environment.NewLine);
             return returnWave;
         }
