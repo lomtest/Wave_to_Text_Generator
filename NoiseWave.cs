50b14d567253602f69248fd899cacbf5254af7ae
Nils Edvardsson <nils@nilsjc.com>
2016-05-25 09:45:00 +0200

    Add files via upload

---
+++ NoiseWave.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaveFormGenerator
+{
+    class NoiseWave : IWaveGenerator
+    {
+        string returnWave { get; set; }
+        public string generate(string maxSamples, string maxLevel, string multiply)
+        {
+            Random rnd = new Random();
+            var _maxLevel = int.Parse(maxLevel);
+            var _lowestLevel = 0 - _maxLevel;
+            var _maxSamples = int.Parse(maxSamples);
+            for (int i = 0; i < _maxSamples; i++)
+            {
+                returnWave += rnd.Next(_lowestLevel, _maxLevel).ToString();
+                returnWave += ",\n";
+            }
+            returnWave = returnWave.Replace("\n", System.Environment.NewLine);
+            return returnWave;
+        }
+    }
+}
