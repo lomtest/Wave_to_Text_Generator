e7f53d7f363e7347f50d89ff34ecee229ed22977
Nils Edvardsson <nils@nilsjc.com>
2016-08-30 14:11:39 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
-﻿using System;
+using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WaveFormGenerator
 {
@@ -15,32 +11,62 @@ namespace WaveFormGenerator
         static void Main(string[] args)
         {
             
-            Console.WriteLine("WaveForm to Textfile Generator \n------------------------------");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Select \nwaveform: \nSine \nSquare \nDefault is random numbers");
-            Console.ForegroundColor = ConsoleColor.Cyan;
+            Write("WaveForm to Textfile Generator \n------------------------------");
+            Color("yellow");
+            Write("Select \nwaveform: \nSine \nSquare \nDefault is random numbers");
+            Color("cyan");
             waveform = Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Numbers of samples:");
-            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Color("green");
+            Write("Numbers of samples:");
+            Color("cyan");
             var numbersSamples = Console.ReadLine();
-            Console.WriteLine("------------------------------");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Top value:");
-            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Write("------------------------------");
+            Color("red");
+            Write("Top value:");
+            Color("cyan");
             var topValue = Console.ReadLine();
-            Console.WriteLine("------------------------------");
-            Console.WriteLine("Frequency Integer Multiply(1, 2, 3... etc) select 1 for single wave cycle");
+
+            Write("------------------------------");
+            Write("Frequency Integer Multiply(1, 2, 3... etc) select 1 for single wave cycle");
             var multiply = Console.ReadLine();
             sw = new StreamWriter(waveform + "Table.txt");
             waveFactory = new WaveFactory(waveform);
             var a = waveFactory.CreateWave();
             sw.Write(a.generate(numbersSamples, topValue, multiply));
             sw.Close();
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Your " + waveform + "waveform is ready to use (in " + waveform + "Table.txt)");
+            Color("white");
+            Write("Your " + waveform + "waveform is ready to use (in " + waveform + "Table.txt)");
             Console.ReadKey();
 
+        }
+
+        static void Write(string line)
+        {
+            Console.WriteLine(line);
+        }
+
+        static void Color(string color)
+        {
+            switch (color.ToLower())
+            {
+                case "white":
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case "yellow":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "cyan":
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
         }
     }
 }
